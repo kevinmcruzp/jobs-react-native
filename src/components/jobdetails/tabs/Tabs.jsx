f4c798509ffdf00125358784677bf05636f9ae77
Kevin Cruz <kevin_mcp@outlook.com>
2024-04-05 13:16:20 -0300

    feat: adicionando o componente e pagina jobdetails

---
+++ src/components/jobdetails/tabs/Tabs.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
+
+import styles from './tabs.style'
+import { SIZES } from '../../../constants'
+
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
+
+  const TabButton = ({ name, activeTab, onHandleSearchType }) => {
+    return (
+      <TouchableOpacity
+        style={styles.btn(name, activeTab)}
+        onPress={onHandleSearchType}
+      >
+        <Text style={styles.btnText(name, activeTab)} >{name}</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={tabs}
+        renderItem={({ item }) => (
+          <TabButton
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
+        keyExtractor={(item) => item}
+      />
+    </View>
+  )
+}
+
+export default Tabs
